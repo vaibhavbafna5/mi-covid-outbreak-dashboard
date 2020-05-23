869283047e4cb6c651dc60909c58104924dbc49a
Michael Rigney <mrigs@umich.edu>
2020-05-22 22:42:50 -0400

    badges

--- src/badges.js
+++ src/badges.js
@@ -5,7 +5,7 @@ class CovidNegativeBadge extends Component {
     render() {
         return (
             <Badge style={{ backgroundColor: "#E8F3EE", color: "#158655", height:"23px", width:"112px", textAlign:"center" }}>
-                <div style={{marginTop:"4px"}}><h8>COVID-Negative</h8></div>
+                <div style={{marginTop:"3px"}}><h8>COVID-Negative</h8></div>
             </Badge>
         );
     };
@@ -15,7 +15,7 @@ class AwaitingResultsBadge extends Component {
     render() {
         return (
             <Badge style={{ backgroundColor: "#FFF7E3", color: "#8A6300", height:"23px", width:"115px", textAlign:"center"}}>
-                <div style={{marginTop:"4px"}}><h8>Awaiting Results</h8></div>
+                <div style={{marginTop:"3px"}}><h8>Awaiting Results</h8></div>
             </Badge>
         );
     };
@@ -25,7 +25,7 @@ class CovidPositiveBadge extends Component {
     render() {
         return (
             <Badge style={{ backgroundColor: "#F8E5E5", color: "#B80000", height:"23px", width:"105px", textAlign:"center"}}>
-                <div style={{marginTop:"4px"}}><h8>COVID-Positive</h8></div>
+                <div style={{marginTop:"3px"}}><h8>COVID-Positive</h8></div>
             </Badge>
         );
     };
@@ -35,7 +35,7 @@ class CovidLikeBadge extends Component {
     render() {
         return (
             <Badge style={{ backgroundColor: "#F3F1F8", color: "#8573BC", height:"23px", width:"82px", textAlign:"center"}}>
-                <div style={{marginTop:"4px"}}><h8>COVID-Like</h8></div>
+                <div style={{marginTop:"3px"}}><h8>COVID-Like</h8></div>
             </Badge>
         )
     }
